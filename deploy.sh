13e257dd9e1f4d0e0acb6abe89b3319bad27ff90
soyrauldev <gq.raul@gmail.com>
2024-06-14 09:47:27 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo "Building Docker images and starting containers..."
+# Usar un argumento para especificar el servicio a reconstruir
+service_name=$1
 
-# Construir las imágenes de Docker
-docker compose --env-file .env up -d --build
+echo "Starting Docker containers..."
+
+if [[ -z "$service_name" ]]; then
+  # Si no se especifica un servicio, levantar todos sin construir
+  echo "No specific service specified. Starting all services without building..."
+  docker-compose --env-file .env up -d
+else
+  # Construir y levantar solo el servicio especificado
+  echo "Building and starting specified service: $service_name"
+  docker-compose --env-file .env up -d --build $service_name
+fi
 
 # Verificar el estado de los servicios
 echo "Waiting for services to become healthy..."
 docker-compose ps
 
 echo "All services are up and running."
-

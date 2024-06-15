d709d722ea49541b87effbd3febd531b3e3f1d53
soyrauldev <gq.raul@gmail.com>
2024-06-14 21:59:42 -0400

    Recolector

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -1,6 +1,6 @@
 
 export const detectHost = async (): Promise<string> => {
   // Utiliza la URL desde el archivo .env, o usa un valor predeterminado si no está disponible
-  const apiUrl = process.env.NEXT_API_URL || "https://rep.uaenorth.cloudapp.azure.com";
+  const apiUrl = "https://rep.uaenorth.cloudapp.azure.com";
   return apiUrl;
 };

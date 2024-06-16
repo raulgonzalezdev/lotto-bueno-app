c98ed2159e9d40be48ec7957fcf3421fa7b02e87
soyrauldev <gq.raul@gmail.com>
2024-06-16 08:55:42 -0400

    new domian app

--- app/main.py
+++ app/main.py
@@ -104,9 +104,9 @@ def get_db():
 # Configuración de CORS
 origins = [
     # "http://localhost",
-    "http://localhost:8000",
-    "http://sas.uaenorth.cloudapp.azure.com:8000/",
-    # "http://localhost:8000",
+    "http://sas.uaenorth.cloudapp.azure.com:8000",
+    "http://localhost:8000/",
+    # "http://sas.uaenorth.cloudapp.azure.com:8000",
     # "http://lot.uaenorth.cloudapp.azure.com",
     # "http://localhost:8002", 
     # "http://localhost:8003", 
@@ -116,7 +116,7 @@ origins = [
     # "http://localhost:3002",
     # "http://localhost:3001",
     "https://7103.api.greenapi.com",
-    # "http://localhost:8000/api/settings",
+    # "http://sas.uaenorth.cloudapp.azure.com:8000/api/settings",
     # "https://lotto-bueno-app-tbsd.vercel.app/"
 ]
 

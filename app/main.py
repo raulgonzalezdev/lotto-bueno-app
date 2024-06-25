5b45976727079536152fbcafdfecf8a4de659315
soyrauldev <gq.raul@gmail.com>
2024-06-25 05:41:51 -0400

    Version Lotto Bueno

--- app/main.py
+++ app/main.py
@@ -107,8 +107,8 @@ origins = [
     "http://localhost:8000",
     "http://localhost:3000",
     "https://7103.api.greenapi.com",
-    "http://lottobueno.store",
-    "https://lottobueno.store"
+    "http://lottobueno.com",
+    "https://lottobueno.com"
 ]
 
 app.add_middleware(

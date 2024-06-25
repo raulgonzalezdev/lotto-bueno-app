301927a26323507d27d3f8800456315669973cd1
soyrauldev <gq.raul@gmail.com>
2024-06-25 12:02:36 -0400

    cambio

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"g1uOglkdJ84R3/VMQFRyK+X4OsvEKIApRgxqg7HcS/c=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"G3XtCuKdozBQtoChmGSucWubMFbpHCR4QQVGihjoge8=\"}"

fd79b6710d62c86e9b7b18b71ae829e051f16434
soyrauldev <gq.raul@gmail.com>
2024-06-25 12:12:18 -0400

    ccs

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"G3XtCuKdozBQtoChmGSucWubMFbpHCR4QQVGihjoge8=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vqDrSr2LMTtpVyDU5mtpgGnWXTrDmsl3S4/UYmf0bqM=\"}"

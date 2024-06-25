e21f5e61f6c13555624dbdd2b7ee5fdb83e74cd4
soyrauldev <gq.raul@gmail.com>
2024-06-25 09:44:31 -0400

    cambios dominio

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+ONZw+H9J481WwIjf0hCZuJ118czCtda6J/F5GHrPSo=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"/pfz8tqPvqwEnaK1mKGjg8y+TY/cl+JpQKoXBVOI8OU=\"}"

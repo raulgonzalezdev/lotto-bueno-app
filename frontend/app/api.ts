b9838e7e01607b2b2fe4333694e0fb969bfe5b61
raulgonzalezdev <gq.raul@gmail.com>
2025-04-05 08:05:40 -0400

    nuevo cambio

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -1,7 +1,8 @@
 export const detectHost = async (): Promise<string> => {
-  // Utiliza la URL desde el archivo .env, o usa un valor predeterminado si no está disponible
+  // Forzar HTTPS y asegurar el formato correcto de la URL
   const apiUrl = process.env.HOST || "https://applottobueno.com";
+  const url = apiUrl.startsWith('https://') ? apiUrl : `https://${apiUrl}`;
   // Eliminar cualquier barra diagonal al final de la URL
-  return apiUrl.replace(/\/+$/, '');
+  return url.replace(/\/+$/, '');
 };
 

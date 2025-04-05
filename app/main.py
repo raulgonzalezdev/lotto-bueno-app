6a5f6cadce52a4dfde2a2d3acfe6d0c96d998379
raulgonzalezdev <gq.raul@gmail.com>
2025-04-05 08:31:10 -0400

    solo domionio

--- app/main.py
+++ app/main.py
@@ -127,15 +127,17 @@ def get_db():
 # Configuración de CORS
 origins = [
     "https://applottobueno.com",
-    "http://applottobueno.com",
+    #"http://applottobueno.com",
     "https://www.applottobueno.com",
-    "http://www.applottobueno.com",
-    "http://applottobueno.com:8000",
-    "https://applottobueno.com:8000",
-    "http://34.134.166.180",
-    "http://34.134.166.180:8000",
-    "http://localhost:3000",
-    "http://localhost:8000"
+    "https://7103.api.greenapi.com",
+     "https://wa.me"
+    #"http://www.applottobueno.com",
+    #"http://applottobueno.com:8000",
+   # "https://applottobueno.com:8000",
+    #"http://34.134.166.180",
+   # "http://34.134.166.180:8000",
+   # "http://localhost:3000",
+   # "http://localhost:8000"
 ]
 
 
